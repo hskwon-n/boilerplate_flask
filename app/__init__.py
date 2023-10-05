5ca89373512ac249b5285a8cbf9fdeb15fadc322
권회성 <hskwon@nemodigm.com>
2023-10-05 15:16:44 +0900

    flask-sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,18 @@ from http import HTTPStatus
 from flask import Flask
 from werkzeug.exceptions import NotFound
 
+from app.database import db
+
 
 def configure_app(app: Flask, test_config):
     if test_config is None:
         app.config.from_object("config.Config")
     else:
         app.config.from_mapping(test_config)
+
+
+def initialize_extensions(app: Flask):
+    db.init_app(app)
 
 
 def route_app(app: Flask):
@@ -21,6 +27,8 @@ def create_app(test_config=None) -> Flask:
     app = Flask(__name__)
 
     configure_app(app, test_config)
+
+    initialize_extensions(app)
 
     route_app(app)
 
--- config.py
+++ config.py
@@ -1,2 +1,15 @@
+from os import environ
+from sqlalchemy import URL
+
+
 class Config:
     DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = URL.create(
+        "mysql",
+        username=environ.get("MYSQL_USERNAME"),
+        password=environ.get("MYSQL_PASSWORD"),
+        host=environ.get("MYSQL_HOST"),
+        port=int(environ.get("MYSQL_PORT", 3306)),
+        database=environ.get("MYSQL_DATABASE"),
+    ).render_as_string()
